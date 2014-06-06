737fa535cd5568a9c09482498eee31cec79b6cc9
Ben Fuller <benjamintf1@gmail.com>
2014-06-06 16:56:54 -0600

    first commit

---
+++ ChessGame/ResetBoard.java
@@ -0,0 +1,22 @@
+package ChessGame;
+import java.io.File;
+
+public class ResetBoard {
+
+	/**
+	 * This should reset the "game board" and create any files as needed. 
+	 * @param args
+	 * This should not have anything in it, Arguments will be ignored
+	 */
+	public static void main(String[] args) {
+		File folder = new File(".");
+		
+		for(String file: folder.list()){
+			if 
+			System.out.println(file);
+		}
+		
+		
+	}
+
+}
