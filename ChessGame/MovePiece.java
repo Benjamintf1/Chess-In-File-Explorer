6560ed6d488d1ecca3d70a9b381ce61b37d9d9b4
Ben Fuller <benjamintf1@gmail.com>
2014-06-12 23:23:40 -0600

    added some structure

--- ChessGame/MovePiece.java
+++ ChessGame/MovePiece.java
@@ -1,11 +1,22 @@
 package chessGame;
 
 public class MovePiece {
-
+	private static char[][] board;
+	private static char[][] boardcolor = {{'b','w','b', 'w', 'b', 'w', 'b', 'w'},
+										  {'w', 'b','w','b', 'w', 'b', 'w', 'b'},
+										  {'b','w','b', 'w', 'b', 'w', 'b', 'w'},
+										  {'w', 'b','w','b', 'w', 'b', 'w', 'b'},
+										  {'b','w','b', 'w', 'b', 'w', 'b', 'w'},
+										  {'w', 'b','w','b', 'w', 'b', 'w', 'b'},
+										  {'b','w','b', 'w', 'b', 'w', 'b', 'w'},
+										  {'w', 'b','w','b', 'w', 'b', 'w', 'b'}     };
 	public static void main(String[] args) {
+		board = new char[8][8];
+		String hello = "hello";
 		for (String s: args){
 			System.out.println(s);
 		}
+		System.out.println(hello.charAt(1));
 
 	}
 
